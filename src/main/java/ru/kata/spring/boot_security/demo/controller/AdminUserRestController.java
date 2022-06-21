416d9ea894fc192e82404693e1a81673424d395f
daniilkhomenko <daniil.khomenko@yahoo.com>
2022-06-21 23:33:41 +0300

    ResponseEntity added

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminUserRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminUserRestController.java
@@ -1,6 +1,8 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
@@ -15,26 +17,27 @@ public class AdminUserRestController {
     private final UserService userService;
 
     @GetMapping("/user")
-    public User getUserByUsername (Principal principal) {
-        return userService.findUserByEmail(principal.getName());
+    public ResponseEntity<User> getUserByUsername (Principal principal) {
+        User user = userService.findUserByEmail(principal.getName());
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @GetMapping("/admin")
-    public List<User> userList(){
-        return userService.getAllUsers();
+    public ResponseEntity<List<User>> userList(){
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
 
 //    @PostMapping("/admin")
 //    @PutMapping("/admin")
     @RequestMapping(value = "/admin", method = {RequestMethod.PUT, RequestMethod.POST})
-    public User createUpdateUser(@RequestBody User user){
+    public ResponseEntity<User> createUpdateUser(@RequestBody User user){
         userService.createUpdate(user);
-        return user;
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @DeleteMapping("/admin/{id}")
-    public List<User> deleteUser(@PathVariable Long id) {
-        userService.delete(id);
-        return userService.getAllUsers();
+    public ResponseEntity<List<User>> deleteUser(@PathVariable Long id) {
+            userService.delete(id);
+            return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
 }
